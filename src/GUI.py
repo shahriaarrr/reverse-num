c4cccc697011a7f961be0c4e8dc4ae635a879b9e
shahriaarrr <75499598+shahriaarrr@users.noreply.github.com>
2021-02-20 16:36:59 +0330

    create WINDOW class and sample view

--- src/GUI.py
+++ src/GUI.py
@@ -7,3 +7,27 @@ def main(n):
         y = y * 10 + (x % 10)
         x = x // 10
     print(y)
+
+class WINDOW():
+    def __init__(self):
+        self.win = Tk()
+
+        self.config_title = 'reverse number'
+        self.config_width = 250
+        self.confing_heigt = 250
+        self.config_resizable = [False, False]
+        self.config_background = '#F36A05'
+
+        self.Load_Configs()
+
+
+
+        mainloop()
+
+    def  Load_Configs(self):
+        self.win.title(self.config_title)
+        self.win.geometry(str(self.config_width) + 'x' + str(self.confing_heigt))
+        self.win.resizable(self.config_resizable[0], self.config_resizable[1])
+        self.win.config(bg = self.config_background)
+
+app = WINDOW()
