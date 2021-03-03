40380d24bda5a3653a859cda27a4a3f4a2ef8653
shahriaarrr <75499598+shahriaarrr@users.noreply.github.com>
2021-03-03 13:11:43 +0330

    Edit ((main)) function

--- src/GUI.py
+++ src/GUI.py
@@ -1,12 +1,12 @@
 from tkinter import *
 
-def main(n):
+def main(n, lbl):
     x = n
     y = 0
     while(x > 0):
         y = y * 10 + (x % 10)
         x = x // 10
-    print(y)
+    lbl.config(text=str(y))
 
 class WINDOW():
     def __init__(self):
